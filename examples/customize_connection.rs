888b65de52a5eb0ec89a923a99392cdaea867d36
Augustus Mayo <gusmayo@gmail.com>
2024-12-09 17:02:01 -0600

    Update bb8 to 0.9

--- examples/customize_connection.rs
+++ examples/customize_connection.rs
@@ -1,7 +1,9 @@
 //! An example showing how to cutomize connections while using pooling.
 
+use std::future::Future;
+use std::pin::Pin;
+
 use async_bb8_diesel::{AsyncSimpleConnection, Connection, ConnectionError};
-use async_trait::async_trait;
 use diesel::pg::PgConnection;
 
 #[derive(Debug)]
@@ -9,13 +11,14 @@ struct ConnectionCustomizer {}
 
 type DieselPgConn = Connection<PgConnection>;
 
-#[async_trait]
 impl bb8::CustomizeConnection<DieselPgConn, ConnectionError> for ConnectionCustomizer {
-    async fn on_acquire(&self, connection: &mut DieselPgConn) -> Result<(), ConnectionError> {
-        connection
-            .batch_execute_async("please execute some raw sql for me")
-            .await
-            .map_err(ConnectionError::from)
+    fn on_acquire<'a>(&'a self, connection: &'a mut DieselPgConn) -> Pin<Box<dyn Future<Output = Result<(), ConnectionError>> + Send + 'a>> {
+        Box::pin(async move {
+            let res = connection
+                .batch_execute_async("please execute some raw sql for me")
+                .await;
+            res.map_err(ConnectionError::from)
+        })
     }
 }
 
